f7134ad31aee7f665664293a9fe85dc42f14893e
EwItsDarion <131904023+EwItsDarion@users.noreply.github.com>
2023-09-12 01:11:15 -0500

    Completion of Prototype 1

---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float forwardInput;
+    public float horizontalInput;
+    public float turnSpeed;
+
+
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+    }
+}
---
+++ Prototype 1/Assets/Scripts/PlayerLoseOnFall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Prototype1
+{
+    public class PlayerLoseOnFall : MonoBehaviour
+    {
+        void Update()
+        {
+            if (transform.position.y < -10)
+            {
+                ScoreManager.gameOver = true;
+            }
+
+        }
+    }
+}
